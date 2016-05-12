b3bd73b9bc6981614b450cbdd3ff46a99ed5c852
Andrew Aldridge <i80and@foxquill.com>
2016-05-12 14:36:56 -0400

    Misc. tweaks and improvements

--- crispy-goggles.js
+++ crispy-goggles.js
@@ -6,7 +6,7 @@ function click(page, selector) {
         try {
             const element = document.querySelector(selector)
             const bounds = element.getBoundingClientRect()
-            return [bounds.left, bounds.top]
+            return [bounds.left + window.scrollY, bounds.top + window.scrollY]
         } catch(err) {}
     }, selector)
 
@@ -21,16 +21,16 @@ function highlight(page, selector) {
     const result = page.evaluate(function(selector) {
         try {
             const element = document.querySelector(selector)
-            const bounds = element.getBoundingClientRect();
+            const bounds = element.getBoundingClientRect()
             const highlight = document.createElement('div')
             highlight.className = '__phantom-highlight'
             highlight.style.position = 'absolute'
-            highlight.style.top = bounds.top + 'px'
-            highlight.style.left = bounds.left + 'px'
+            highlight.style.top = bounds.top + window.scrollY + 'px'
+            highlight.style.left = bounds.left + window.scrollX + 'px'
             highlight.style.width = bounds.width + 'px'
             highlight.style.height = bounds.height + 'px'
             highlight.style['background-color'] = 'rgba(128, 128, 128, 0.5)'
-            highlight.style['z-index'] = 10000000
+            highlight.style['z-index'] = element.style['z-index'] + 1
             document.body.appendChild(highlight)
             return true
         } catch(err) {
@@ -56,7 +56,8 @@ function setValue(selector, value) {
 }
 
 const page = require('webpage').create()
-page.viewportSize = { width: 1024, height: 768 }
+page.viewportSize = { width: 1200, height: 768 }
+page.clipRect = { top: 0, left: 0, width: 1200, height: 768 }
 page.onError = function(msg, trace) {
     console.log(msg);
     trace.forEach(function(item) {
@@ -68,6 +69,10 @@ var name = ''
 var stepNum = 0
 const variables = {}
 function doSteps(steps, callback) {
+    const nextStep = function() {
+        doSteps(steps.slice(1), callback)
+    }
+
     if(steps.length === 0) { return callback() }
     if(!steps[0].trim() || steps[0].match(/^\s*#/)) {
         return doSteps(steps.slice(1), callback)
@@ -82,8 +87,8 @@ function doSteps(steps, callback) {
         phantom.exit()
     }
 
-    const command = matches[1]
-    const origArg = matches[2]
+    const command = matches[1].trim()
+    const origArg = matches[2].trim()
 
     // Substitute variables
     const arg = origArg.replace(/(\$+){(\S+)}/g, function(match, dollars, varname) {
@@ -98,10 +103,10 @@ function doSteps(steps, callback) {
 
     if(command === 'name') {
         name = arg
-        return doSteps(steps.slice(1), callback)
+        return nextStep()
     } else if(command === 'click') {
         click(page, arg)
-        return doSteps(steps.slice(1), callback)
+        return nextStep()
     } else if(command === 'load') {
         page.open(arg, function(status) {
             if(status !== 'success') {
@@ -109,18 +114,18 @@ function doSteps(steps, callback) {
                 phantom.exit()
             }
 
-            return doSteps(steps.slice(1), callback)
+            return nextStep()
         })
     } else if(command === 'highlight') {
         highlight(page, arg)
-        return doSteps(steps.slice(1), callback)
+        return nextStep()
     } else if(command === 'unhighlight') {
         unhighlight(page)
-        return doSteps(steps.slice(1), callback)
+        return nextStep()
     } else if(command === 'render') {
         const filename = '' + name + '-' + arg + '.png'
         page.render(filename)
-        return doSteps(steps.slice(1), callback)
+        return nextStep()
     } else if(command === 'set') {
         const setMatches = arg.match(/(\S+)\s*=\s*(.*)/)
         if(setMatches === null) {
@@ -130,11 +135,12 @@ function doSteps(steps, callback) {
         const varname = setMatches[1]
         const varvalue = setMatches[2]
         variables[varname] = varvalue
-        return doSteps(steps.slice(1), callback)
+        return nextStep()
     } else if(command === 'wait') {
+        const timeout = arg? parseInt(arg) : 10000
         setTimeout(function() {
-            return doSteps(steps.slice(1), callback)
-        }, parseInt(arg))
+            return nextStep()
+        }, parseInt(timeout))
     } else if(command === 'setValue') {
         const setValueMatches = arg.match(/([^,]+),\s*(.*)/)
         if(setValueMatches === null) {
@@ -145,7 +151,34 @@ function doSteps(steps, callback) {
         const value = setValueMatches[2]
 
         setValue(selector, value)
-        return doSteps(steps.slice(1), callback)
+        return nextStep()
+    } else if(command === 'scroll') {
+        if(arg === 'reset') {
+            page.scrollPosition = {
+              top: 0,
+              left: 0
+            }
+
+            return nextStep()
+        }
+
+        const vertPos = page.evaluate(function(selector) {
+            const element = document.querySelector(selector)
+            if(!element) { return null }
+
+            const bounds = element.getBoundingClientRect()
+            return bounds.top
+        }, arg)
+
+        if(vertPos === null) {
+            throw new Error('Could not select "' + selector + '"')
+        }
+
+        page.scrollPosition = {
+          top: vertPos,
+          left: 0
+        }
+        return nextStep()
     } else {
         console.error('Unknown command "' + command + '"')
         phantom.exit()
